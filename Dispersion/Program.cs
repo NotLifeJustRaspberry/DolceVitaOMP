f38ec6a2a9092c69d6939b2988e6950f5cb066a0
AlinaRinn <nik17042001@rambler.ru>
2022-11-08 00:47:54 +0300

    Create input (source) csv file generator

--- Dispersion/Program.cs
+++ Dispersion/Program.cs
@@ -4,5 +4,29 @@
 // Для распараллеливания использовать OpenMP, использовать весь ресурс параллелизма (параллелить, где можно) и оптимизации компилятора.
 // Построить график зависимости ускорения по сравнению с 1 потоком и сравнить с линейным ускорением.
 
+class Dispersion
+{
+    const int STRINGS = 100;
+    const int COLS = 10;
+    static void FillingCSV(int str, int cols)
+    {
+        Random r = new();
+        StreamWriter sw = new StreamWriter("Input.csv");
+        for (int i = 0; i < str; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                sw.Write(r.Next(65536) - 32768 + ";");
+            }
+            sw.WriteLine();
+        }
+        sw.Close();
+        Console.WriteLine("CSV Filled");
+    }
 
-Console.WriteLine("Hello, World!");
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        FillingCSV(STRINGS, COLS);
+    }
+}
