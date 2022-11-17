47fdab5ead897070bdc2ca0c3c8fbcd62dc8f6ff
AlinaRinn <nik17042001@rambler.ru>
2022-11-17 21:48:27 +0300

    Added test for SplitText

--- Dispersion.Tests/Test.cs
+++ Dispersion.Tests/Test.cs
@@ -52,5 +52,31 @@ namespace Dispersion.Tests
                 Assert.AreEqual(expected[i], actual[i]);
         }
 
+        [TestMethod]
+        public void TestSplitText()
+        {
+            ulong[][] expected = new ulong[4][]
+            {
+                new ulong[] { 105, 10, 56 },
+                new ulong[] { 56, 235, 876 },
+                new ulong[] { 234, 675, 234 },
+                new ulong[] { 345 ,11, 554 }
+            };
+
+            List<string> input = new();
+            foreach (var line in expected)
+            {
+                StringBuilder temp = new ();
+                foreach (var element in line)
+                    temp.Append(element + " ");
+                input.Add(temp.ToString());
+            }
+
+            ulong[][] actual = Dispersion.SplitText(input.ToArray());
+
+            for (int i = 0; i < expected.Length; i++)
+                for (int j = 0; j < expected[0].Length; j++)
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+        }
     }
 }
